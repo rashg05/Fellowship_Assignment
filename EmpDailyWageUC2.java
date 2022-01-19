3b5d5d092558e518bf132ce8c3d299980c8f849a
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-19 19:30:35 +0530

    Added: calculate Emp Wage program if present or absent

---
+++ EmpDailyWageUC2.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class EmpDailyWageUC2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " +empWage);
+	}
+
+}
