0a547d1d694716f331af262902a1113bcd65f13f
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-19 22:57:07 +0530

    added: calculating wages till number of working days

---
+++ EmpWageBuilderWhileUC6.java
@@ -0,0 +1,35 @@
+package assignments;
+
+public class EmpWageBuilderWhileUC6 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				switch (empCheck) {
+					case IS_PART_TIME: 
+						empHrs = 4;
+						break;
+					case IS_FULL_TIME:
+						empHrs = 8;
+						break;
+					default:
+						empHrs = 0;
+				}
+				totalEmpHrs += empHrs;
+				System.out.println("Day#: " + totalWorkingDays + "Employee hour: " +empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Employee Wage: " + totalEmpWage);
+	}
+
+}
